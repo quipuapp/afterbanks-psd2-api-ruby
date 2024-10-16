90a658906b073c9f7335bfdca26329b235649a8b
Florent Lemaitre <flemaitre@sellsy.com>
2024-10-16 17:20:07 +0200

    fix test for version

--- spec/afterbanks_psd2/version_spec.rb
+++ spec/afterbanks_psd2/version_spec.rb
@@ -2,6 +2,6 @@ require "spec_helper"
 
 describe "AfterbanksPSD2::VERSION" do
   it "is the proper one" do
-    expect(AfterbanksPSD2::VERSION).to eq("0.1.0")
+    expect(AfterbanksPSD2::VERSION).to eq("0.1.1")
   end
 end
